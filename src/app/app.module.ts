71002bd179b8eaa5daa5636c32dfba486a6b069b
NadaYoussef <eng_nada.youssef@yahoo.com>
2025-02-05 21:40:13 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component'; 
+import { LogListComponent } from './log-list/log-list.component'; 
+import { LogDetailsComponent } from './log-details/log-details.component'; 
+import { HttpClientModule } from '@angular/common/http'; 
+import { AppRoutingModule } from './app-routing.module'; 
 import { MatTableModule } from '@angular/material/table';
+import { FormsModule } from '@angular/forms'; /
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatButtonModule } from '@angular/material/button';
-import { AppComponent } from './app.component';
-import { LogListComponent } from './components/log-list/log-list.component';
-import { LogService } from './services/log.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     LogListComponent,
+    LogDetailsComponent 
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatDatepickerModule,
-    MatButtonModule
+    AppRoutingModule, 
+    MatTableModule, 
+    FormsModule, 
+    MatPaginatorModule, 
   ],
-  providers: [LogService],
-  bootstrap: [AppComponent]
+  providers: [],
+  bootstrap: [AppComponent] 
 })
 export class AppModule { }
