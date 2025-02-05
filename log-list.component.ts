50ffe7f4bd4fb4e1054e236eaf0e4bb2a799b255
NadaYoussef <eng_nada.youssef@yahoo.com>
2025-02-05 12:13:49 +0200

    Create log-list.component.ts

---
+++ log-list.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { LogService } from '../../services/log.service';
+import { LogEntry, LogFilter } from '../../models/log-entry.model';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+
+@Component({
+  selector: 'app-log-list',
+  templateUrl: './log-list.component.html',
+  styleUrls: ['./log-list.component.css'],
+})
+export class LogListComponent implements OnInit {
+  logs: LogEntry[] = [];
+  displayedColumns: string[] = ['timestamp', 'service', 'level', 'message'];
+  dataSource: MatTableDataSource<LogEntry> = new MatTableDataSource();
+  filters: LogFilter = {};  // Filter object
+
+  constructor(private logService: LogService) {}
+
+  ngOnInit(): void {
+    this.getLogs();  // Initial fetch of logs
+  }
+ 
+  getLogs(): void {
+    this.logService.getLogs(this.filters).subscribe((logs) => {
+      this.logs = logs;
+      this.dataSource.data = this.logs;
+    });
+  }
+
+  applyFilter(service: string, level: string, startTime: string, endTime: string): void {
+    this.filters = {
+      service,
+      level,
+      startTime: startTime,
+      endTime: endTime,
+    };
+    this.getLogs();
+  }
+
+  
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+  @ViewChild(MatSort) sort: MatSort | undefined;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+}
